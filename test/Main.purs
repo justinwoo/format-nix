ba6e8b2fbd71e0544f5120da2e669bdaa185c434
justinwoo <moomoowoo@gmail.com>
2019-03-28 17:06:03 +0100

    test update with output

--- test/Main.purs
+++ test/Main.purs
@@ -2,28 +2,32 @@ module Test.Main where
 
 import Prelude
 
-import Data.Traversable (traverse_)
+import Data.Array as Array
+import Data.Traversable (traverse, traverse_)
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Effect.Class.Console (log)
 import FormatNix (TreeSitterParser, children, mkParser, nixLanguage, parse, printExpr, readNode, rootNode)
 import Node.Encoding (Encoding(..))
-import Node.FS.Aff (readTextFile)
+import Node.FS.Aff (readTextFile, writeTextFile)
 import Node.Path (FilePath)
 
 parser :: TreeSitterParser
 parser = mkParser nixLanguage
 
-processInput :: FilePath -> Aff Unit
+processInput :: FilePath -> Aff String
 processInput filepath = do
   input <- readTextFile UTF8 filepath
   let node = rootNode $ parse parser input
   let nodes = readNode <$> children node
   log $ "printing " <> filepath <> ":"
-  traverse_ (log <<< printExpr) nodes
+  let output = Array.intercalate "\n" $ printExpr <$> nodes
+  log output
   log ""
+  pure output
 
 main :: Effect Unit
 main = launchAff_ do
-  processInput "test/build.nix"
-  processInput "test/fetch-github.nix"
+  results <- traverse processInput [ "test/build.nix", "test/fetch-github.nix" ]
+  let output = Array.intercalate "\n\n" results <> "\n"
+  writeTextFile UTF8 "test/output.txt" output
--- test/build.nix
+++ test/build.nix
@@ -4,15 +4,14 @@
 let
   dynamic-linker = pkgs.stdenv.cc.bintools.dynamicLinker;
 
-  patchelf = libPath :
-    if pkgs.stdenv.isDarwin
-      then ""
-      else
-        ''
-          chmod u+w $PURS
-          patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $PURS
-          chmod u-w $PURS
-        '';
+  patchelf = libPath : if pkgs.stdenv.isDarwin
+    then ""
+    else
+      ''
+        chmod u+w $PURS
+        patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $PURS
+        chmod u-w $PURS
+      '';
 
 in pkgs.stdenv.mkDerivation rec {
   name = "purs-simple";
@@ -38,14 +37,15 @@ in pkgs.stdenv.mkDerivation rec {
   libPath = pkgs.lib.makeLibraryPath buildInputs;
   dontStrip = true;
 
-  installPhase = ''
-    mkdir -p $out/bin
-    PURS="$out/bin/purs"
+  installPhase =
+    ''
+      mkdir -p $out/bin
+      PURS="$out/bin/purs"
 
-    install -D -m555 -T purs $PURS
-    ${patchelf libPath}
+      install -D -m555 -T purs $PURS
+      ${patchelf libPath}
 
-    mkdir -p $out/etc/bash_completion.d/
-    $PURS --bash-completion-script $PURS > $out/etc/bash_completion.d/purs-completion.bash
-  '';
+      mkdir -p $out/etc/bash_completion.d/
+      $PURS --bash-completion-script $PURS > $out/etc/bash_completion.d/purs-completion.bash
+    '';
 }
